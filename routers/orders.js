42d8b35886b7d551bba5bfa0ccafd345081f9630
arbab.muhammad <arbab.muhammad@evampsaanga.com>
2021-11-09 18:42:26 +0500

    orders api implemented

--- routers/orders.js
+++ routers/orders.js
@@ -1,23 +1,55 @@
 const { Order } = require("../models/order");
 const { OrderItem } = require("../models/order-item");
+const { Product } = require("../models/product");
 const express = require("express");
 const router = express.Router();
 router.get(`/`, async (req, res) => {
-  const orderList = await Order.find();
+  const orderList = await Order.find()
+    .populate("user", "name")
+    .sort({ dateOrdered: -1 });
   if (!orderList) return res.status(404).json({ success: false });
   res.send(orderList);
 });
+router.get("/:id", async (req, res) => {
+  const order = await Order.findById(req.params.id)
+    .populate("user", "name")
+    .populate({
+      path: "orderItems",
+      populate: {
+        path: "product",
+        populate: "category",
+      },
+    });
+  if (!order) return res.status(404).json({ success: false });
+  res.send(order);
+});
+router.put("/:id", async (req, res) => {
+  const order = await Order.findByIdAndUpdate(
+    req.params.id,
+    {
+      status: req.body.status,
+    },
+    { new: true }
+  );
+  if (!order) return res.status(404).json({ success: false });
+  res.send(order);
+});
+router.delete("/:id", async (req, res) => {
+  const order = await Order.findByIdAndRemove(req.params.id).then(
+    async (order) => {
+      if (order) {
+        await order.orderItems.map(async (orderItem) => {
+          await OrderItem.findByIdAndRemove(orderItem);
+        });
+        return res.status(200).json({ success: true });
+      } else {
+        return res.status(404).json({ success: false });
+      }
+    }
+  );
+  if (!order) return res.status(404).json({ success: false });
+});
 router.post("/", async (req, res) => {
-  // const orderItemsIds = Promise.all(
-  //   req.body.orderItems.map(async (item) => {
-  //     let newOrderItem = new OrderItem({
-  //       quantity: item.quantity,
-  //       product: item.product,
-  //     });
-  //     newOrderItem = await newOrderItem.save();
-  //     return newOrderItem._id;
-  //   })
-  // );
   const orderItemsIds = Promise.all(
     req.body.orderItems.map(async (item) => {
       let newOrderItem = new OrderItem({
@@ -28,8 +60,20 @@ router.post("/", async (req, res) => {
       return newOrderItem._id;
     })
   );
+
   const orderItemsIdsResolved = await orderItemsIds;
-  console.log(orderItemsIdsResolved);
+
+  const totalPrices = await Promise.all(
+    orderItemsIdsResolved.map(async (orderItemId) => {
+      const orderItem = await OrderItem.findById(orderItemId).populate(
+        "product",
+        "price"
+      );
+      const totalPrice = orderItem.quantity * orderItem.product.price;
+      return totalPrice;
+    })
+  );
+  const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
   let order = new Order({
     orderItems: orderItemsIdsResolved,
     shippingAddress1: req.body.shippingAddress1,
@@ -39,7 +83,7 @@ router.post("/", async (req, res) => {
     country: req.body.country,
     phone: req.body.phone,
     status: req.body.status,
-    totalPrice: req.body.totalPrice,
+    totalPrice: totalPrice,
     user: req.body.user,
   });
   order = await order.save();
